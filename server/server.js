2be0ef37339a244ba16dd7d7f8b9fc0fb583f346
wzqwangyoucao <1737481406@qq.com>
2018-08-15 22:10:03 +0800

    项目第一天

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const userRouter = require('./user.js')
+const userRouter = require('./user')
 
 
 const app = express();
-app.use('./user',userRouter)//开启中间件
+app.use('/user',userRouter)//开启中间件
 
 app.listen(9093,function(){
     console.log('Node app start port at 9093');
